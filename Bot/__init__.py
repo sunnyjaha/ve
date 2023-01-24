0cd258b631167b65dc889f0153581885e1f26562
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-24 13:32:17 +0530

    Update __init__.py
    Signed-off-by: anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,23 +6,23 @@ if os.path.exists('error.log'):
         
 #<-----------LOGGING------------>
 logging.basicConfig(level=logging.INFO, filename='error.log')
-LOG = logging.getLogger("Bot by @soheru")
+LOG = logging.getLogger("Bot by @am_robots")
 LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'your bot token') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 456789)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
-MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '5931646626:AAGWH3AYL-qIx7GkQhmWeyp0p5DT3tDwlk8') #BOT Token Add
+API_ID = int(os.environ.get('API_ID', 16734393)) #Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', 'e49a46ce63b238c429d618c4aa37f52e')# Telgram App hash  
+OWNER_ID = int(os.environ.get('OWNER_ID', 5543917190)
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://mirror:mirror@cluster0.fuouswc.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001787652537)
+BOT_NAME = os.environ.get('BOT_NAME', 'Video_Compressor_AmRoBot')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
         encoder  = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
-        LOG.info('❤️ Bot Connected Created By Github @soheru || Telegram @sohailkhan_indianime ')
+        LOG.info('❤️ Bot Connected Created By Github @Am_Robots || Telegram @Amrobots_Bots ')
     except Exception as e:
         LOG.warn(f'😞 Error While Connecting To Bot\nCheck Errors: {e}')
         sys.exit()       
