8ae61bda7cb30481ff40fae5f8fccf1e3299758b
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-24 13:35:46 +0530

    Update __init__.py
    Signed-off-by: anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -12,11 +12,11 @@ LOG.setLevel(level=logging.INFO)
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '5931646626:AAGWH3AYL-qIx7GkQhmWeyp0p5DT3tDwlk8') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 16734393)) #Telgram Api id
+API_ID = int(os.environ.get('API_ID', '16734393')) #Telgram Api id
 APP_HASH = os.environ.get('APP_HASH', 'e49a46ce63b238c429d618c4aa37f52e')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 5543917190)
+OWNER_ID = int(os.environ.get('OWNER_ID', '5543917190')
 MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://mirror:mirror@cluster0.fuouswc.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001787652537)
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", "-1001787652537")
 BOT_NAME = os.environ.get('BOT_NAME', 'Video_Compressor_AmRoBot')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
